824e092a53aa47924abb808025d234460e7c74af
susmoysaha6 <susmoysaha6@gmail.com>
2022-12-26 16:44:12 +0600

    server started

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
@@ -10,3 +9,11 @@ const port = process.env.PORT || 5000;
 // middle wares
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send(' server is running')
+})
+
+app.listen(port, () => {
+    console.log(`server running on ${port}`);
+})
